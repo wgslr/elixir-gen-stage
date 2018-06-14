f8bece6a4006a69606ae56ddfd4e0da7e0c6c2a8
Wojciech Geisler <wojciech.geisler@gmail.com>
2018-06-14 23:00:06 +0200

    Create app skeleton with mix

---
+++ lib/gen_stage_sample.ex
@@ -0,0 +1,18 @@
+defmodule GenStageSample do
+  @moduledoc """
+  Documentation for GenStageSample.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GenStageSample.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gen_stage_sample_test.exs
@@ -0,0 +1,8 @@
+defmodule GenStageSampleTest do
+  use ExUnit.Case
+  doctest GenStageSample
+
+  test "greets the world" do
+    assert GenStageSample.hello() == :world
+  end
+end
